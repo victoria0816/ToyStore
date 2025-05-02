75dc199ca7cc93f195595fc127d355e7507311c1
victoria0816 <victoria.damamme@gmail.com>
2025-05-02 15:08:15 +0200

    Correcting add to cart trigger

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,23 +6,23 @@ const renderBadge = () => {
 }
 
 const listenToAdd = (buttons) => {
-buttons.forEach((button) => {
-button.addEventListener('click', (event) => {
-const { id, name, price } = event.currentTarget.dataset
-if (cartLS.exists(id)) {
-cartLS.quantity(id, 1)
-} else {
-cartLS.add({ id, name, price })
-}
-dataLayer.push({
-event: 'add_to_cart',
-product_id: id,
-product_name: name,
-product_price: Number(price)
+  buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const { id, name, price } = event.currentTarget.dataset
+      if (cartLS.exists(id)) {
+          cartLS.quantity(id, 1)
+      } else {
+      cartLS.add({ id, name, price })
+      }
+      dataLayer.push({
+        event: 'add_to_cart',
+        product_id: id,
+        product_name: name,
+        product_price: Number(price)
 })
 })
 })
-
+ }
 const cartItemsListeners = () => {
   const addToCartButtons = document.querySelectorAll('.cart .add-to-cart')
   listenToAdd(addToCartButtons)
